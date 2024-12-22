3ee441689ae0c11e0ef1baea1528419c624cfef9
Ronaldo Bicca <ronaldo.bicca@gmail.com>
2024-12-22 20:06:16 -0300

    Cap 10

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,10 @@ import {MessagesService} from "../messages/messages.service";
 import {catchError, from, throwError} from "rxjs";
 import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
 
+type Counter = {
+  value: number
+};
+
 @Component({
     selector: 'home',
     imports: [
@@ -19,5 +23,34 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  counter = signal<Counter>({ value: 100});
+  values = signal<number[]>([0]);
+  cont = signal(0);
+  tenX = computed(()=> {
+    return this.cont() * 10;
+  });
+
+  increment(){
+    // this.counter.set(this.counter() +1);
+
+    //this.counter.update(v => v+1);
+
+    // this.counter.update(v => ({
+    //   ...v,
+    //   value: v.value + 1
+    // }));
+
+    this.cont.update(v => v +1);
+  }
+
+  append(){
+      this.values.update(v =>
+        [
+          ...v,
+          v[v.length -1] + 1
+        ]
+      );
+  }
 
 }
